ebd4a80f2f0d949d562f5b59bc5996259cf658e1
KevinPeinado33 <kevinpeinado@upeu.edu.pe>
2020-01-24 20:42:51 -0500

    conexion a base de datos

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
+const mysqlConnection = require('../database');
+
 router.get('/', (req, res) => {
-    res.render('index.html', { title: 'Primera Pagina' });
+    //res.render('index.html', { title: 'Primera Pagina' });
+    mysqlConnection.query('select * from persona', (err, rows, fields) => {
+        if(!err) { 
+            console.log(rows);
+        } else {
+            console.log(err);
+        }
+    });
 });
 
 router.get('/contact', (req, res) => {
