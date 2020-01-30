56803ac82c48962104253d3f31892f2716834814
KevinPeinado33 <kevinpeinado@upeu.edu.pe>
2020-01-30 10:25:32 -0500

    construyendo vista prinpical

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,14 +4,7 @@ const router = express.Router();
 const mysqlConnection = require('../database');
 
 router.get('/', (req, res) => {
-    //res.render('index.html', { title: 'Primera Pagina' });
-    mysqlConnection.query('select * from persona', (err, rows, fields) => {
-        if(!err) { 
-            console.log(rows);
-        } else {
-            console.log(err);
-        }
-    });
+    res.render('index.html', { title: 'Home' });
 });
 
 router.get('/contact', (req, res) => {
